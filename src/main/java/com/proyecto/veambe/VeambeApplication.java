90b673836f8a051bfceeb0c798a12d8816633ad1
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-09 13:30:18 +0200

    Initial commit

---
+++ src/main/java/com/proyecto/veambe/VeambeApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.veambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeambeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeambeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/veambe/VeambeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.veambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeambeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
