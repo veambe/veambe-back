a48bdc0bf2cfe48aa3a782436fd5c2c6d1dc4528
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-19 09:10:04 +0200

    Add artwork controller

--- src/main/java/com/proyecto/veambe/controller/ArtworkController.java
+++ src/main/java/com/proyecto/veambe/controller/ArtworkController.java
@@ -1,5 +1,8 @@
 package com.proyecto.veambe.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,23 +13,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
-@RequestMapping("api/v1/work")
+@RequestMapping("api/v1/trabajo")
 public class ArtworkController {
 
   private final ArtworkService artworkService;
 
-  public ArtworkController(ArtworkService artworkService){
+  public ArtworkController(ArtworkService artworkService) {
     this.artworkService = artworkService;
   }
 
   @PostMapping("/{adminId}")
-  public ResponseEntity<Object> createArtwork(@PathVariable Integer adminId, @RequestBody Artwork artwork){
-  Integer categoryId = artwork.getCategory().getId(); 
-   return artworkService.createArtwork(artwork, adminId, categoryId);
+  public ResponseEntity<Object> createArtwork(@PathVariable Integer adminId, @RequestBody Artwork artwork) {
+    Integer categoryId = artwork.getCategory().getId();
+    return artworkService.createArtwork(artwork, adminId, categoryId);
   }
 
+  @GetMapping()
+  public List<Artwork> getAllArtworks(){
+    return artworkService.getAllArtworks();
+  }
+  
+  
+
+  @GetMapping("/categoria/{categoryName}")
+  public List<Artwork> getArtworksByCategory(@PathVariable String categoryName) {
+    return artworkService.getArtworksByCategory(categoryName);
+  }
+
+  @GetMapping("/obra/{artworkId}")
+  public Optional<Artwork> getArtworkById(@PathVariable Integer artworkId) {
+    return artworkService.getArtworkById(artworkId);
+  }
 
 }
