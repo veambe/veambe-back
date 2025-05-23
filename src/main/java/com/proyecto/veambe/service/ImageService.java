4179c9547da28f99aa8cc3254b480afb6cd21700
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-23 08:47:59 +0200

    Delete comments

--- src/main/java/com/proyecto/veambe/service/ImageService.java
+++ src/main/java/com/proyecto/veambe/service/ImageService.java
@@ -38,20 +38,17 @@ public class ImageService {
     Artwork artwork = artworkRepository.findById(artworkId)
         .orElseThrow(() -> new RuntimeException("No se encontró la obra"));
 
-    // esto lo guarda en el disco local
-    String filename = UUID.randomUUID() + "_" + file.getOriginalFilename(); // esto genera un nombre nuevo que lo añade
-                                                                            // al del archivo para que no se repitan los
-                                                                            // nombres
-    Files.createDirectories(Paths.get(uploadDir));
+    String filename = UUID.randomUUID() + "_" + file.getOriginalFilename(); 
+                                                                            
+                                                                            
     Path filePath = Paths.get(uploadDir, filename);
     Files.createDirectories(filePath.getParent());
     Files.copy(file.getInputStream(), filePath);
 
-    // aquí guardamos en la base de datos
-    Image image = new Image(); // creamos una nueva entidad imagen
-    image.setFilePath("uploads/" + filename); // asignamos una ruta 'relativa'
-    image.setArtwork(artwork); // juntamos la imagen al artwork
-    return imageRepository.save(image); // guardamos en la bd
+    Image image = new Image(); 
+    image.setFilePath("uploads/" + filename); 
+    image.setArtwork(artwork); 
+    return imageRepository.save(image); 
   }
 
   public Image addImageToArtwork(Integer artworkId, Image image) {
