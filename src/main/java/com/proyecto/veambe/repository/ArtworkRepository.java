f148169b843fe1280f4f366e80fa57aab5c16ce5
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-06-04 10:54:47 +0200

    Restructure repository layer

--- src/main/java/com/proyecto/veambe/repository/ArtworkRepository.java
+++ src/main/java/com/proyecto/veambe/repository/ArtworkRepository.java
@@ -3,11 +3,9 @@ package com.proyecto.veambe.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.proyecto.veambe.model.Artwork;
 
-@Repository
 public interface ArtworkRepository extends JpaRepository<Artwork, Integer> {
   List<Artwork> findByCategoryName(String categoryName);
 
--- src/main/java/com/proyecto/veambe/repository/ImageRepository.java
+++ src/main/java/com/proyecto/veambe/repository/ImageRepository.java
@@ -3,11 +3,9 @@ package com.proyecto.veambe.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.proyecto.veambe.model.Image;
 
-@Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
   List<Image> findByArtworkId(Integer artworkId);
 }
